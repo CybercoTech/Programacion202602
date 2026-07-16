bc17d853eebe785e29bf9af962d94b363228c99c
jose vasquez <josevasqueze069@gmail.com>
2026-07-16 11:25:13 -0500

    third commit

---
+++ hola_mundo.py
@@ -0,0 +1,13 @@
+#mi primer script.py
+Print("hola, Github")  
+Print("hello, Github")  
+Print("hola, Github")
+Print("ciao, Github")  
+Print("hallo, Github")  
+Print("hola, Github")
+Print("nihao, Github")    
+
+
+
+
+
